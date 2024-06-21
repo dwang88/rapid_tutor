acf5d24c43baecc98a84e347bd2ba6cad7c2e80f
dwang88 <davidwang1089@gmail.com>
2024-06-21 02:06:27 -0700

    split controls & tutors into two sections, add filter by subjects

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -15,6 +15,10 @@ const Faq = () => {
       answer: "You can book a tutor by browsing our list of available tutors, selecting one that fits your needs, clicking the tutor card, and paying through Paypal's secure checkout options. We also accept credit and debit card. Once your payment has gone through, you will be redirected to a Zoom meeting where you will be automatically allocated to your selected tutor."
     },
     {
+      question: 'How do I get tutored?',
+      answer: "After payment, you will be redirected to a Zoom meeting on our website. You will then be put into a breakout room with your booked tutor."
+    },
+    {
       question: 'What payment methods are accepted?',
       answer: 'We accept various payment methods including credit cards, debit cards, and PayPal. We are working on implementing more payment options.'
     },
@@ -29,22 +33,18 @@ const Faq = () => {
     {
         question: "I didn't get a Zoom link",
         answer: 'Please close all payment popups including Paypal checkout, Paypal Pay Later, and Debit or Credit options. If you are still not redirected to a Zoom meeting page, please contact us. We will respond ASAP.'
-      },
-    {
-      question: 'Who We Are',
-      answer: 'Rapid Tutor was founded by two college students who firsthand experienced the prohibitive costs associated with private tutoring. Driven by their belief that education should be accessible to all, they created Rapid Tutor to democratize access to quality education. Our platform empowers learners from all backgrounds to access personalized tutoring and educational resources that were once out of reach.'
-    },
-    {
-      question: 'Our Mission',
-      answer: 'Our mission is to provide affordable, high-quality tutoring services to help students achieve their academic goals. We are dedicated to fostering an inclusive learning environment, leveraging innovative technology to deliver personalized education, and continuously improving our platform to meet the diverse needs of our learners. By prioritizing accessibility and excellence, we strive to empower every student to reach their full potential and succeed academically.'
-    },
-    {
-      question: 'Our Values',
-      answer: '1. Affordable 1-on-1 Tutoring\n2. Commitment to Excellence\n3. Personalized Learning\n4. Integrity and Transparency'
     },
     {
       question: 'What does Rapid Tutor offer?',
       answer: 'We offer affordable, quality tutoring to help students crush their academic goals. Through innovative technology and a welcoming environment, we personalize learning for everyone. Our goal? Empower YOU to shine in school!'
+    },
+    {
+      question: 'How can I become a tutor on Rapid Tutor?',
+      answer: 'If you are passionate about teaching and want to join our platform as a tutor, we welcome you to apply through our website. Navigate to the contact section and submit your qualifications. Our team will review your application and reach out to you with further instructions.'
+    },
+    {
+      question: 'Is Rapid Tutor available worldwide?',
+      answer: 'Yes! Rapid Tutor allows students to connect with a tutor via Zoom, an online meeting platform. '
     }
   ];
 
--- src/components/TutorList.js
+++ src/components/TutorList.js
@@ -35,14 +35,7 @@ const TutorList = () => {
       image: 'https://i.imgur.com/uXuR6wu.png',
       description: "As a highly motivated English language instructor with a Bachelor's degree in Secondary English Education, I can help you achieve your writing goals.  My extensive classroom experience allows me to offer engaging and effective instruction tailored to your specific needs.  Let's work together to unlock your full potential as a writer and communicator.",
       availability: [
-        { day: 'Monday', start: '09:00', end: '17:00' },
-        { day: 'Tuesday', start: '10:00', end: '23:00' },
-        { day: 'Wednesday', start: '09:00', end: '17:00' },
-        { day: 'Thursday', start: '10:00', end: '23:00' },
-        { day: 'Friday', start: '09:00', end: '17:00' },
-        { day: 'Saturday', start: '10:00', end: '23:00' },
-        { day: 'Sunday', start: '10:00', end: '23:00' }
-
+        { day: 'Monday', start: '00:00', end: '23:59' }, { day: 'Tuesday', start: '00:00', end: '23:59' }, { day: 'Wednesday', start: '00:00', end: '23:59' }, { day: 'Thursday', start: '00:00', end: '23:59' }, { day: 'Friday', start: '00:00', end: '23:59' }, { day: 'Saturday', start: '00:00', end: '23:59' }, { day: 'Sunday', start: '00:00', end: '23:59' }
       ]
     },
     {
@@ -57,8 +50,7 @@ const TutorList = () => {
       image: 'https://t3.ftcdn.net/jpg/01/92/16/04/360_F_192160468_2ev2JYmocXi7pxbBiPsfNEVwDqmTTLYL.jpg',
       description: 'Experienced math tutor with a focus on algebra, geometry, and calculus.',
       availability: [
-        { day: 'Tuesday', start: '02:21', end: '23:59' },
-        { day: 'Thursday', start: '13:00', end: '18:00' }
+        { day: 'Monday', start: '00:00', end: '23:59' }, { day: 'Tuesday', start: '00:00', end: '23:59' }, { day: 'Wednesday', start: '00:00', end: '23:59' }, { day: 'Thursday', start: '00:00', end: '23:59' }, { day: 'Friday', start: '00:00', end: '23:59' }, { day: 'Saturday', start: '00:00', end: '23:59' }, { day: 'Sunday', start: '00:00', end: '23:59' }
       ]
     },
     {
@@ -90,17 +82,17 @@ const TutorList = () => {
       image: 'https://www.care.com/s/d/aws/photo/1080X1080/92/46024792_t03Wzz746abHAqn4ixO7C769tVe6d010',
       description: "Hey there! I'm your one-stop shop for crushing college admissions.  I help students like you master the SAT and ACT, brainstorm and write killer college essays, and navigate the sometimes-confusing college prep process.  Let's work together to make your dream school a reality!",
       availability: [
-        { day: 'Monday', start: '14:00', end: '18:00' },
-        { day: 'Wednesday', start: '00:00', end: '23:00' }
+        { day: 'Monday', start: '00:00', end: '23:59' }, { day: 'Tuesday', start: '00:00', end: '23:59' }, { day: 'Wednesday', start: '00:00', end: '23:59' }, { day: 'Thursday', start: '00:00', end: '23:59' }, { day: 'Friday', start: '00:00', end: '23:59' }, { day: 'Saturday', start: '00:00', end: '23:59' }, { day: 'Sunday', start: '00:00', end: '23:59' }
       ]
     }
   ];
-
   const [sortOrder, setSortOrder] = useState('default');
   const [searchTerm, setSearchTerm] = useState('');
   const [selectedTutor, setSelectedTutor] = useState(null);
   const [availableTutors, setAvailableTutors] = useState([]);
   const [unavailableTutors, setUnavailableTutors] = useState([]);
+  const [filteredTutors, setFilteredTutors] = useState([]);
+  const [selectedSubject, setSelectedSubject] = useState('');
   const paypalRef = useRef();
 
   const handleSortOrderChange = (e) => {
@@ -109,6 +101,10 @@ const TutorList = () => {
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
+  };
+
+  const handleSubjectClick = (subject) => {
+    setSelectedSubject(subject);
   };
 
   const isTutorAvailable = (availability) => {
@@ -166,9 +162,12 @@ const TutorList = () => {
     }
   }, [selectedTutor]);
 
-  const filteredTutors = availableTutors.filter(tutor =>
-    tutor.description.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  useEffect(() => {
+    const filterBySearchTerm = availableTutors.filter(tutor =>
+      tutor.description.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredTutors(filterBySearchTerm);
+  }, [searchTerm, availableTutors]);
 
   const sortedTutors = filteredTutors.sort((a, b) => {
     if (sortOrder === 'lowest') {
@@ -180,6 +179,10 @@ const TutorList = () => {
     }
   });
 
+  const filteredBySubjectTutors = sortedTutors.filter(tutor =>
+    tutor.subject.toLowerCase().includes(selectedSubject.toLowerCase())
+  );
+
   const handleCardClick = (tutor) => {
     setSelectedTutor(tutor);
   };
@@ -188,93 +191,161 @@ const TutorList = () => {
     setSelectedTutor(null);
   };
 
-  return (
-    <div className="tutor-list">
-      <Navbar />
-      <h1>Available Tutors</h1>
-      <div className="controls">
-        <div className="sort-filter">
-          <label>
-            Sort by Price:
-            <select className='input' value={sortOrder} onChange={handleSortOrderChange}>
-              <option className='droptext' value="default">Default</option>
-              <option className='droptext' value="lowest">Lowest to Highest</option>
-              <option className='droptext' value="highest">Highest to Lowest</option>
-            </select>
-          </label>
+  const TutorCard = ({ tutor }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [shouldTruncateAvailability, setShouldTruncateAvailability] = useState(false);
+    const availabilityRef = useRef();
+
+    useEffect(() => {
+      if (availabilityRef.current) {
+        setShouldTruncateAvailability(availabilityRef.current.scrollHeight > 40);
+      }
+    }, []);
+
+    const toggleDescription = (e) => {
+      e.stopPropagation();
+      setIsExpanded(!isExpanded);
+    };
+
+    const shouldTruncateDescription = tutor.description.length > 200;
+
+    return (
+      <div className="tutor-card" onClick={() => handleCardClick(tutor)}>
+        <img src={tutor.image} alt={`${tutor.name}'s profile`} className="tutor-image" />
+        <div className="tutor-name">{tutor.name + " • $" + tutor.rate + "/hr"}</div>
+        <div className="tutor-subject"><strong>Subjects:</strong> {tutor.subject}</div>
+        <div className="tutor-location"><strong>Location:</strong> {tutor.location}</div>
+        <div className="tutor-experience"><strong>Experience:</strong> {tutor.exp}</div>
+        <div className="tutor-description">
+          <strong>About:</strong> {isExpanded || !shouldTruncateDescription ? tutor.description : `${tutor.description.slice(0, 200)}...`}
+          {shouldTruncateDescription && (
+            <span className="more-text" onClick={toggleDescription}>
+              {isExpanded ? ' Show less' : ' Show more'}
+            </span>
+          )}
         </div>
-        <div className="search-filter">
-          <label>
-            Search by Description:
-            <input
-              className='input'
-              type="text"
-              value={searchTerm}
-              onChange={handleSearchChange}
-              placeholder="Enter subject, location, etc"
-            />
-          </label>
+        <div className="tutor-availability" ref={availabilityRef}>
+          <strong>Available:</strong>
+          {isExpanded || !shouldTruncateAvailability ? tutor.availability.map(slot => (
+            <span key={`${slot.day}-${slot.start}`}>
+              {slot.day} {slot.start}-{slot.end}<br />
+            </span>
+          )) : tutor.availability.slice(0, 2).map(slot => (
+            <span key={`${slot.day}-${slot.start}`}>
+              {slot.day} {slot.start}-{slot.end}<br />
+            </span>
+          ))}
+          {shouldTruncateAvailability && (
+            <span className="more-text" onClick={toggleDescription}>
+              {isExpanded ? ' Show less' : ' Show more'}
+            </span>
+          )}
+        </div>
+        <button
+          className="form-button"
+          onClick={(e) => {
+            e.stopPropagation();
+            setSelectedTutor(tutor);
+          }}
+        >
+          Book Tutor
+        </button>
+      </div>
+    );
+  };
+
+  const uniqueSubjects = [...new Set(tutors.flatMap(tutor => tutor.subject.split(',').map(sub => sub.trim())))];
+
+  return (
+    <div>
+    <Navbar />
+    <div className="tutor-list">
+      <div className="controls-tutorsection-wrapper">
+        <div className="controls">
+          <div className="sort-filter">
+            <label>
+              Sort by Price:
+              <select className='input' value={sortOrder} onChange={handleSortOrderChange}>
+                <option className='droptext' value="default">Default</option>
+                <option className='droptext' value="lowest">Lowest to Highest</option>
+                <option className='droptext' value="highest">Highest to Lowest</option>
+              </select>
+            </label>
+          </div>
+
+          <div className="search-filter">
+            <label>
+              Search by Description:
+              <input
+                className='input'
+                type="text"
+                value={searchTerm}
+                onChange={handleSearchChange}
+                placeholder="Enter subject, location, etc"
+              />
+            </label>
+          </div>
+
+          <div className="subject-buttons">
+            {uniqueSubjects.map(subject => (
+              <button
+                key={subject}
+                onClick={() => handleSubjectClick(subject)}
+                className={`subject-button ${selectedSubject === subject ? 'active' : ''}`}
+              >
+                {subject}
+              </button>
+            ))}
+            <button
+              onClick={() => handleSubjectClick('')}
+              className={`subject-button ${selectedSubject === '' ? 'active' : ''}`}
+            >
+              Show All
+            </button>
+          </div>
+        </div>
+
+        <div className='tutorsection'>
+          <h1>Available Tutors</h1>
+          {filteredBySubjectTutors.length > 0 ? (
+            <div className="tutors">
+              {filteredBySubjectTutors.map((tutor) => (
+                <TutorCard key={tutor._id} tutor={tutor} />
+              ))}
+            </div>
+          ) : (
+            <p className='notutors'>No tutors available</p>
+          )}
+          {unavailableTutors.length > 0 && (
+            <>
+              <h1>Unavailable Tutors</h1>
+              <div className="tutors">
+                {unavailableTutors.map((tutor) => (
+                  <div className="tutor-card unavailable" key={tutor._id}>
+                    <div className="tutor-content">
+                      <h2>{tutor.name + " • $" + tutor.rate + "/hr"}</h2>
+                      <p><strong>Subjects:</strong> {tutor.subject}</p>
+                      <p><strong>About:</strong> {tutor.description}</p>
+                      <p><strong>Available:</strong> {tutor.availability.map(slot => (
+                        <span key={`${slot.day}-${slot.start}`}>
+                          {slot.day} {slot.start}-{slot.end}<br />
+                        </span>
+                      ))}</p>
+                      <button className="form-button" disabled>
+                        Book Tutor
+                      </button>
+                    </div>
+                    <img src={tutor.image} alt={`${tutor.name}'s profile`} className="tutor-image" />
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
         </div>
       </div>
-      {sortedTutors.length > 0 ? (
-        <div className="tutors">
-          {sortedTutors.map((tutor) => (
-            <div className="tutor-card" key={tutor._id} onClick={() => handleCardClick(tutor)}>
-  <img src={tutor.image} alt={`${tutor.name}'s profile`} className="tutor-image" />
-  <div className="tutor-name">{tutor.name + " • $" + tutor.rate + "/hr"}</div>
-  <div className="tutor-subject"><strong>Subjects:</strong> {tutor.subject}</div>
-  <div className="tutor-location"><strong>Location:</strong> {tutor.location}</div>
-  <div className="tutor-experience"><strong>Experience:</strong> {tutor.exp}</div>
-  <div className="tutor-description"><strong>About:</strong> {tutor.description}</div>
-  <div className="tutor-availability"><strong>Available:</strong> {tutor.availability.map(slot => (
-    <span key={`${slot.day}-${slot.start}`}>
-      {slot.day} {slot.start}-{slot.end}<br />
-    </span>
-  ))}</div>
-  <button
-    className="form-button"
-    onClick={(e) => {
-      setSelectedTutor(tutor);
-    }}
-  >
-    Book Tutor
-  </button>
-</div>
 
-          ))}
-        </div>
-      ) : (
-        <p className='notutors'>No tutors available</p>
-      )}
-      {unavailableTutors.length > 0 && (
-        <>
-          <h1>Unavailable Tutors</h1>
-          <div className="tutors">
-            {unavailableTutors.map((tutor) => (
-              <div className="tutor-card unavailable" key={tutor._id}>
-                <div className="tutor-content">
-                  <h2>{tutor.name + " • $" + tutor.rate + "/hr"}</h2>
-                  <p><strong>Subjects:</strong> {tutor.subject}</p>
-                  <p><strong>About:</strong> {tutor.description}</p>
-                  <p><strong>Available:</strong> {tutor.availability.map(slot => (
-                    <span key={`${slot.day}-${slot.start}`}>
-                      {slot.day} {slot.start}-{slot.end}<br />
-                    </span>
-                  ))}</p>
-                  <button
-                    className="form-button"
-                    disabled
-                  >
-                    Book Tutor
-                  </button>
-                </div>
-                <img src={tutor.image} alt={`${tutor.name}'s profile`} className="tutor-image" />
-              </div>
-            ))}
-          </div>
-        </>
-      )}
       <Modal show={!!selectedTutor} handleClose={handleCloseModal} tutor={selectedTutor} />
+    </div>
     </div>
   );
 };
